01b6176f8287b1a9662ec55d7476f6c91b96d0ca
Ibukun16 <ib.agunbiade18@gmail.com>
2024-11-15 15:20:29 +0000

    Update tasks

---
+++ 0x02-Session_authentication/README.md
@@ -0,0 +1,1 @@
+This project directory contains solutions to practice project tasks on Session authentication, an extension of Basic authentication project
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Authentication module for API
+"""
+import re
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Authentication class for Auth.
+
+    Args:
+        path (str): _description_
+        excluded_paths (List[str]): _description_
+
+    Returns:
+            bool: _description_
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checking if a path requires authentication.
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+                bool: _description_
+        """
+        if path is not None and excluded_paths is not None:
+            for exclude in map(lambda x: x.strip(), excluded_paths):
+                fmt = ''
+                if exclude[-1] == '*':
+                    fmt = '{}.*'.format(exclude[0:-1])
+                elif exclude[-1] == '/':
+                    fmt = '{}/*'.format(exclude[0:-1])
+                else:
+                    fmt = '{}/*'.format(exclude)
+                if re.match(fmt, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Get the authorization header field details from the request
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+                str: _description_
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user details from the request
+        """
+        return None
