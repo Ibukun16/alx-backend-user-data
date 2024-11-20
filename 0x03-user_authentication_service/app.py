fb7965225cd67dbd2d0687f3ee847437ee1e845e
Ibukun16 <ib.agunbiade18@gmail.com>
2024-11-20 21:14:34 +0000

    Update all changes to project tasks and return correct output

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,6 +95,7 @@ def get_reset_password_token() -> str:
     if token is None:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """PUT update with the reset password
@@ -107,11 +108,9 @@ def update_password() -> str:
     new_pswd = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, new_pswd)
-        password_changed = True
         return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
-
 
 
 if __name__ == "__main__":
---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""Main module that run end-to-end (E2E)
+integration test for app.py
+"""
+import requests
+
+
+EMAIL = "guillame@holberton.io"
+PASSWD = "b410u"
+NEW_PASSWD = "t4rt1fl3tt3"
+BASE_URL = "http://0.0.0.0:5000"
+
+
+def register_user(email: str, password: str) -> None:
+    """A function that test the a user register
+    """
+    url = f"{BASE_URL}/users"
+    body = {'email': email, 'password': password}
+    resp = requests.post(url, data=body)
+    assert resp.status_code == 200
+    assert resp.json() == {"email":email, "message": "user created"}
+    resp = request.post(url, data=body)
+    assert resp.status_code == 400
+    assert resp.json() == {"message": "email already registered"}
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """A  function that test wrong log in 
+    """
+    url = f"{BASE_URL}/sessions"
+    body = {'email': email, 'password': password}
+    resp = requests.post(url, data=body)
+    assert resp.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """A function that test for correct log in
+    """
+    url = f"{BASE_URL}/sessions"
+    body = {'email': email, 'password': password}
+    resp = requests.post(url, data=body)
+    assert resp.status_code == 200
+    assert resp.json() == {"email": email, "message": "logged in"}
+    return resp.cookies.get('session_id')
+
+
+def profile_unlogged() -> None:
+    """A function that test retrieving
+    profile information whilst logged out
+    """
+    url = f"{BASE_URL}/profile"
+    body = {'email': email, 'password': password}
+    cookies = {'session_id': session_id}
+    resp = requests.get(url, cookies=cookies)
+    assert resp.status_code == 200
+    assert "email" in resp.json()
+
+
+def log_out(session_id: str) -> None:
+    """A function that tests logging out of a session
+    """
+    url = f"{BASE_URL}/sessions"
+    cookies = {'session_id': session_id}
+    resp = requests.delete(url, cookies=cookies)
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "Bienvenue"}
+
+
+def reset_password_token(email: str) -> str:
+    """A function that test requesting a password reset
+    """
+    url = f"{BASE_URL}/reset_password"
+    body = {'email': email}
+    resp =  requests.post(url, data=body)
+    assert resp.status_code == 200
+    assert "email" in resp.json()
+    assert resp.json()["email"] == email
+    assert "reset_token" in resp.json()
+    return resp.json().get('reset_token')
+
+
+def update_password(email: str, reset_token: str, new_password:str) -> None:
+    """A function that test updating user password
+    """
+    url = f"{BASE_URL}/reset_password"
+    body = {'email': email,
+            'reset_token': reset_token,
+            'new_password': new_password,
+    }
+    resp = requests.put(url, data=body)
+    assert resp.status_code == 200
+    assert resp.json() == {"email": email, "message": "Password updated"}
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_unlogged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
+
